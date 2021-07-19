4c0a82003bdc4d9863ebbc6059681b6517f63c91
Defmc <defmc0101@gmail.com>
2021-07-19 16:38:46 -0300

    Adicionadas macros referente ao estado em que o vetor se encontra

--- src/fvec.h
+++ src/fvec.h
@@ -12,6 +12,10 @@
 
 #define fvec_allocated_size(fvec) ((size_t)(fvec->alloc_adr - fvec->start))
 
+#define fvec_is_full(fvec) (fvec->end == fvec->alloc_adr)
+
+#define fvec_is_empty(fvec) (fvec->end == fvec->start)
+
 #define fvec_slice(from, to, type, srt_idx, end_idx) (FVec##type*)malloc(sizeof(FVec##type));\
  to->start = from->start + srt_idx;\
 to->alloc_adr = from->alloc_adr;\
